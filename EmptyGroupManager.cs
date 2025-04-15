60de80b10a2e458d1eb27df6717b537b532e1bdc
awsrcld04 <akil.woolfolk@gmail.com>
2025-04-15 16:01:49 -0400

    Removed funcProgramRegistryTag from funcGetFuncCatchCode

--- EmptyGroupManager.cs
+++ EmptyGroupManager.cs
@@ -474,7 +474,6 @@ namespace EmptyGroupManager
             dCatchTable.Add("funcPrintParameterSyntax", "f3");
             dCatchTable.Add("funcParseCmdArguments", "f4");
             dCatchTable.Add("funcProgramExecution", "f5");
-            dCatchTable.Add("funcProgramRegistryTag", "f6");
             dCatchTable.Add("funcCreateDSSearcher", "f7");
             dCatchTable.Add("funcCreatePrincipalContext", "f8");
             dCatchTable.Add("funcCheckNameExclusion", "f9");
